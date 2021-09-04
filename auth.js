642045e69132ec09090d4e8b24529fee0efca24e
rmedx <65210483+rmedx@users.noreply.github.com>
2021-09-04 19:08:17 -0400

    edit

--- auth.js
+++ auth.js
@@ -31,31 +31,35 @@ module.exports = (app, myDataBase) => {
           })
         }
     ));
-    passport.use(new GitHubStrategy({clientID: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_SECRET, callbackURL: 'https://young-lake-39153.herokuapp.com/auth/github/callback'}, (accessToken, refreshToken, profile, cb) => {
-        myDataBase.findOneAndUpdate(
-            { id: profile.id },
-            {
-            $setOnInsert: {
-                id: profile.id,
-                name: profile.displayName || 'John Doe',
-                photo: profile.photos[0].value || '',
-                email: Array.isArray(profile.emails)
-                ? profile.emails[0].value
-                : 'No public email',
-                created_on: new Date(),
-                provider: profile.provider || ''
-            },
-            $set: {
-                last_login: new Date()
-            },
-            $inc: {
-                login_count: 1
-            }
-            },
-            { upsert: true, new: true },
-            (err, doc) => {
-            return cb(null, doc.value);
-            }
+    passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+            callbackURL: process.cwd() + '/auth/github/callback'
+        }, (accessToken, refreshToken, profile, cb) => {
+            myDataBase.findOneAndUpdate(
+                { id: profile.id },
+                {
+                $setOnInsert: {
+                    id: profile.id,
+                    name: profile.displayName || 'John Doe',
+                    photo: profile.photos[0].value || '',
+                    email: Array.isArray(profile.emails)
+                    ? profile.emails[0].value
+                    : 'No public email',
+                    created_on: new Date(),
+                    provider: profile.provider || ''
+                },
+                $set: {
+                    last_login: new Date()
+                },
+                $inc: {
+                    login_count: 1
+                }
+                },
+                { upsert: true, new: true },
+                (err, doc) => {
+                return cb(null, doc.value);
+                }
         );
     }));
 }
