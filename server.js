94f5fda97db51a9c3c3f8c0f6f20b832541f5c4e
rmedx <65210483+rmedx@users.noreply.github.com>
2021-09-07 19:49:47 -0400

    added disconnect listener

--- server.js
+++ server.js
@@ -36,6 +36,11 @@ myDB(async (client) => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
+    });
   });
 }).catch((e) => {
   app.route('/').get((req, res) => {
