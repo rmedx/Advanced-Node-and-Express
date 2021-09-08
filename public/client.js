4abcc30dae92e17173f1706bd1d7aa88da2ebd28
rmedx <65210483+rmedx@users.noreply.github.com>
2021-09-08 14:59:50 -0400

    edit

--- public/client.js
+++ public/client.js
@@ -4,13 +4,16 @@ $(document).ready(function () {
   let socket = io();
   socket.on('user', (data) => {
     $('#num-users').text(data.currentUsers) + ' users online';
-    let message = data.name + (data.connected ? ' has joined the chat' : ' has left the chat.');
+    let message = data.username + (data.connected ? ' has joined the chat' : ' has left the chat.');
     $('#messages').append($('<li>').html('<b>' + message + '/b>')); 
   });
+  socket.on('chat message', (data) => {
+    $('messages').append($('<li>').html(data.username + ": " + message));
+  })
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
-
+    socket.emit('chat message', messageToSend);
     $('#m').val('');
     return false; // prevent form submit from refreshing page
   });
--- server.js
+++ server.js
@@ -52,14 +52,17 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on('connection', socket => {
     ++currentUsers;
-    io.emit('user', {name: socket.request.user.username, currentUsers, connected: true});
+    io.emit('user', {username: socket.request.user.username, currentUsers, connected: true});
     console.log('user ' + socket.request.user.username + ' connected');
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
-      io.emit('user', {name: socket.request.user.username, currentUsers, connected: false});
+      io.emit('user', {username: socket.request.user.username, currentUsers, connected: false});
     });
   });
+  io.on('chat message', message => {
+    io.emit('chat message', {username: socket.request.username, message});
+  })
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', {
